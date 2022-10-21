3dbfe56c78f83ac3379a3be154a54d17d31fc4c7
Richard <richard.brown0807@gmail.com>
2022-10-20 20:23:38 -0500

    commit tree d08be5a143dda8abbd9c44ce5482af14dad067f0

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution here
+const drivers1 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+const drivers2 = drivers1.join(" ").toLowerCase().split(" ")
+
+const findMatching = drivers1.filter(driver =>
+  driver === "Timmy");
+
+console.log(findMatching);
+
+
+const drivers3 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+ 
+  const matchName = drivers3.filter(str => str.name === "Bobby")
+
+  //console.log(matchName)
+  
